d018a8f82c8e95ebba8db90f73f2b059ce6a9834
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-08-09 09:25:51 +0100

    Initial Commit

---
+++ pseudo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pseudo
+//
+//  Created by Javax on 09/08/2018.
+//  Copyright © 2018 Javax Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
