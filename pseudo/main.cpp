6275a4908dad2ccfc69898d50b057c64cda16a03
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2018-11-01 16:59:57 +0000

    Wrote documentation and ran final build

--- pseudo/main.cpp
+++ pseudo/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, const char * argv[]) {
 	vector<Variable> variables;
 	vector<Marker> markers;
 #ifdef DEBUG
-	string codeFile = "/Repository of Things/Coding/AIs/pseudo/pseudo/demo.pso";
+	string codeFile = "/Repository of Things/Coding/AIs/pseudo/pseudo/code5.pso";
 	Interpreter inter = Interpreter(readEntireTextFile(codeFile), &mm, &variables, &markers);
 #else
 	Interpreter inter = Interpreter(readEntireTextFile(argv[1]), &mm, &variables, &markers);
